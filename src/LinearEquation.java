2362b4f4d13cb4d54622b5822a487f3d28b8efb6
BT_1N3_25 <sample@bths.edu>
2023-10-16 11:02:19 -0400

    Second commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,18 +1,28 @@
 public class LinearEquation {
-    double slope;
+    String slope;
     double yIntercept;
     double distanceBetweenPoints;
     String firstCoord;
     String secondCoord;
     public LinearEquation(int firstX, int firstY, int secondX, int secondY) {
-        slope = (double) (secondY-firstY) / (secondX-firstX);
-        yIntercept = firstY - (firstX * slope);
+        double tempSlope = (double) (secondY-firstY) / (secondX-firstX);
+        if (tempSlope == (double) ((secondY-firstY) / (secondX-firstX))) {
+            slope = Integer.toString((secondY-firstY) / (secondX-firstX));
+        } else {
+            slope = (secondY-firstY) + "/" + (secondX-firstX);
+        }
+        yIntercept = firstY - (firstX * tempSlope);
         distanceBetweenPoints = Math.sqrt(Math.pow(secondX-firstX, 2) + Math.pow(secondY - firstY, 2));
         firstCoord = "(" + firstX + ", " + firstY + ")";
         secondCoord = "(" + secondX + ", " + secondY + ")";
     }
 
     public String lineInfo() {
-        return "The 2 points are: " + firstCoord + " and " + secondCoord + "\nThe equation of the line between these points is: y = " + slope + "x" + " + " + yIntercept + "\nThe slope of this line is: " + slope + "\nThe y-intercept of this line is: " + yIntercept + "\nThe distance between these points is " + distanceBetweenPoints;
+        String returnString = "The 2 points are: " + firstCoord + " and " + secondCoord + "\nThe equation of the line between these points is: y = ";
+        if (!slope.equals("1")) {
+            returnString += slope;
+        }
+        returnString += "x" + " + " + yIntercept + "\nThe slope of this line is: " + slope + "\nThe y-intercept of this line is: " + yIntercept + "\nThe distance between these points is " + distanceBetweenPoints;
+        return returnString;
     }
 }
