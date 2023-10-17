be407f15e2ef15451365fa723567fe525ea656c1
BT_1N3_25 <sample@bths.edu>
2023-10-17 11:04:02 -0400

    added stuff

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,28 +1,70 @@
 public class LinearEquation {
-    String slope;
-    double yIntercept;
-    double distanceBetweenPoints;
-    String firstCoord;
-    String secondCoord;
+    private String slopeString;
+    private String firstCoord;
+    private String secondCoord;
+    private int firstX;
+    private int firstY;
+    private int secondX;
+    private int secondY;
     public LinearEquation(int firstX, int firstY, int secondX, int secondY) {
-        double tempSlope = (double) (secondY-firstY) / (secondX-firstX);
-        if (tempSlope == (double) ((secondY-firstY) / (secondX-firstX))) {
-            slope = Integer.toString((secondY-firstY) / (secondX-firstX));
-        } else {
-            slope = (secondY-firstY) + "/" + (secondX-firstX);
-        }
-        yIntercept = firstY - (firstX * tempSlope);
-        distanceBetweenPoints = Math.sqrt(Math.pow(secondX-firstX, 2) + Math.pow(secondY - firstY, 2));
         firstCoord = "(" + firstX + ", " + firstY + ")";
         secondCoord = "(" + secondX + ", " + secondY + ")";
+        this.firstX = firstX;
+        this.firstY = firstY;
+        this.secondX = secondX;
+        this.secondY = secondY;
+        if (slope() == (int) slope()) {
+            slopeString = Integer.toString((secondY-firstY) / (secondX-firstX));
+        } else {
+            boolean isPositive = true;
+            if ((secondY-firstY) < 0) {
+                isPositive = !isPositive;
+            }
+            if ((secondX-firstX) < 0) {
+                isPositive = !isPositive;
+            }
+            slopeString = Math.abs((secondY-firstY)) + "/" + Math.abs((secondX-firstX));
+            if (!isPositive) {
+                slopeString = "-" + slopeString;
+            }
+        }
+    }
+
+    public double Distance() {
+        return roundedToHundredth(Math.sqrt(Math.pow(secondX-firstX, 2) + Math.pow(secondY - firstY, 2)));
+    }
+
+    public double yIntercept() {
+        return firstY - (firstX * slope());
+    }
+
+    public double slope() {
+        return (double) (secondY - firstY) / (secondX - firstX);
+    }
+
+    public String equation() {
+        String stringToReturn = "y = ";
+        if (slope() == 1.0) {
+            stringToReturn += "x + " + yIntercept();
+        } else if (slope() == 0.0) {
+            stringToReturn += yIntercept();
+        } else if (slope() == -1.0) {
+            stringToReturn += "-x + " + yIntercept();
+        } else {
+            stringToReturn += slopeString + "x + " + yIntercept();
+        }
+        return stringToReturn;
     }
 
     public String lineInfo() {
-        String returnString = "The 2 points are: " + firstCoord + " and " + secondCoord + "\nThe equation of the line between these points is: y = ";
-        if (!slope.equals("1")) {
-            returnString += slope;
-        }
-        returnString += "x" + " + " + yIntercept + "\nThe slope of this line is: " + slope + "\nThe y-intercept of this line is: " + yIntercept + "\nThe distance between these points is " + distanceBetweenPoints;
-        return returnString;
+        return "The 2 points are: " + firstCoord + " and " + secondCoord + "\nThe equation of the line between these points is: " + equation()+ "\nThe slope of this line is: " + slope() + "\nThe y-intercept of this line is: " + yIntercept() + "\nThe distance between these points is " + Distance();
+    }
+
+    public double coordinateForX(double x) {
+        return slope() * x + yIntercept();
+    }
+
+    private double roundedToHundredth(double toRound) {
+        return Math.round(toRound * 100) / 100.0;
     }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class LinearEquationLogic {
+    private LinearEquation equation;
     private Scanner scan;
     private String firstCoord;
     private String secondCoord;
@@ -13,29 +14,50 @@ public class LinearEquationLogic {
         scan = new Scanner(System.in);
         firstCoord = null;
         secondCoord = null;
+        equation = null;
     }
 
     public void start() {
-        System.out.println("Welcome to the Linear equation calculator!");
-        getCoordinateInputs();
-        calculateXAndY();
-        LinearEquation equation = new LinearEquation(firstX, firstY, secondX, secondY);
+        initialSetUp();
+        equation = new LinearEquation(firstX, firstY, secondX, secondY);
         System.out.println(equation.lineInfo());
+        calculateYfromX();
     }
 
-    private void getCoordinateInputs() {
-        System.out.print("Enter your first coordinate point: ");
-        firstCoord = scan.nextLine();
-        System.out.print("Enter your second coordinate point: ");
-        secondCoord = scan.nextLine();
-    }
+    private void initialSetUp() {
+        boolean continueLoop = true;
+        System.out.println("Welcome to the Linear equation calculator!");
+        while (continueLoop) {
+            System.out.print("Enter your first coordinate point: ");
+            firstCoord = scan.nextLine();
+            System.out.print("Enter your second coordinate point: ");
+            secondCoord = scan.nextLine();
 
-    private void calculateXAndY() {
-        int firstCommaIndex = firstCoord.indexOf(",");
-        firstX = Integer.parseInt(firstCoord.substring(1, firstCommaIndex));
-        firstY = Integer.parseInt(firstCoord.substring(firstCommaIndex + 2, firstCoord.length() - 1));
-        int secondCommaIndex = secondCoord.indexOf(",");
-        secondX = Integer.parseInt(secondCoord.substring(1, secondCommaIndex));
-        secondY = Integer.parseInt(secondCoord.substring(secondCommaIndex + 2, secondCoord.length() - 1));
+            int firstCommaIndex = firstCoord.indexOf(",");
+            firstX = Integer.parseInt(firstCoord.substring(1, firstCommaIndex));
+            firstY = Integer.parseInt(firstCoord.substring(firstCommaIndex + 2, firstCoord.length() - 1));
+            int secondCommaIndex = secondCoord.indexOf(",");
+            secondX = Integer.parseInt(secondCoord.substring(1, secondCommaIndex));
+            secondY = Integer.parseInt(secondCoord.substring(secondCommaIndex + 2, secondCoord.length() - 1));
+
+            if (firstX == secondX) {
+                System.out.println("The 2 points have the same x value and therefore, this is not a function. Try using different points with different x values.");
+            } else {
+                continueLoop = false;
+            }
+        }
+    }
+    public void calculateYfromX() {
+        boolean continueLoop = true;
+        while (continueLoop) {
+            System.out.print("Enter an x value: ");
+            double x = scan.nextDouble();
+            scan.nextLine();
+            System.out.println("Your new coordinate is: (" + x + ", " + equation.coordinateForX(x) + ")");
+            System.out.print("Would you like to get another coordinate point? (\"n\" to stop): ");
+            if (scan.nextLine().equals("n")) {
+                continueLoop = false;
+            }
+        }
     }
 }
